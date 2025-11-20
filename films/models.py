efaa1a48f13e51fa42ac6ed383f40674901409db
MrEmm6 <Emm6.kern@hotmail.ch>
2025-11-20 14:48:47 +0100

    implémentation de la bdd sur la page principale

--- films/models.py
+++ films/models.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Film(models.Model):
     auteur = models.CharField(max_length=50)
     titre = models.CharField(max_length=50)
     avis = models.TextField()
-    dateVue = models.DateTimeField(default=timezone.now)
+    dateVue = models.DateField(default=timezone.now)
+
+    def get_date_vue(self):
+        return self.dateVue.strftime("%d.%m.%Y")
 
     def __str__(self):
         return self.titre
--- films/views.py
+++ films/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Film
 # Create your views here.
 
 def list_films_seen(request):
-    return render(request, 'films/list_films_seen.html', {})
+    list_films = Film.objects.order_by('-dateVue')
+    return render(request, 'films/list_films_seen.html', {'list_films' : list_films})
