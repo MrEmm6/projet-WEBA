d148518725f3857c1f73ec3d96ad0e8353a103ce
emmanuel <emm6.kern@hotmail.ch>
2025-11-23 17:44:24 +0100

    implémentation ajout et modif de film + intégration d'héritage des templates

--- films/urls.py
+++ films/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.list_films_seen, name='list_films_seen'),
+    path('film/<int:pk>/modif/', views.modif_film, name='modif_film'),
+    path('film/ajout/', views.ajout_film, name='ajout_film'),
 ]
--- films/views.py
+++ films/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from .forms import FilmForm
 from .models import Film
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 # Create your views here.
 
 def list_films_seen(request):
     list_films = Film.objects.order_by('-dateVue')
     return render(request, 'films/list_films_seen.html', {'list_films' : list_films})
+
+def ajout_film(request):
+    if request.method == "POST":
+        form = FilmForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('list_films_seen')
+    else:
+        form = FilmForm()
+    return render(request, 'films/ajout_film.html', {'form': form})
+
+def modif_film(request, pk):
+    post = get_object_or_404(Film, pk=pk)
+    if request.method == "POST":
+        form = FilmForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('list_films_seen')
+    else:
+        form = FilmForm(instance=post)
+    return render(request, 'films/modif_film.html', {'form': form})
